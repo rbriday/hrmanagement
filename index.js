461a97d69fd292efd9cc61938917bf3e9a302060
rbriday <ridaya100@gmail.com>
2026-03-02 11:57:23 +0600

    bdConnection modify

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const dbConnection = require("./config/dbConnection");
 const app = express();
 
 app.use(express.json());
-
-mongoose.connect(process.env.DB_URL).then(() => {
-  console.log("Database connented");
-});
+dbConnection();
 
 app.get("/", (req, res) => {
   res.send("hello developres");
